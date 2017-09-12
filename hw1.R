f0158b6f340918b2d7b2e0f35db26be1fcd2def7
MeganZzz <zj-9988@hotmail.com>
2017-09-12 00:43:42 -0400

    Add files via upload

--- hw1.R
+++ hw1.R
@@ -1,39 +1,61 @@
 library(class)
-path<-'/Users/megan/Documents/STAT315A_stanford/assignment&exam/hw1'
+
+####################################################################################################
+###############     DATA INPUT     #################
+path <- '/Users/megan/Documents/STAT315A_stanford/assignment&exam/hw1'
 setwd(path)
-temp<-readLines("train.txt")
-train<-list()
-m=length(temp)
+temp <- readLines("train.txt")
+train <- list()
+m = length(temp)
+options(digits = 6)
+for (i in 1:m) {
+  train[[i]] = as.numeric(unlist(strsplit(temp[i], " ")))
+}
 rm(temp)
-options(digits=6)
-for (i in 1:m){
-  train[[i]]=as.numeric(unlist(strsplit(temp[i], " ")))
+#p = ncol(train)
+train <- matrix(unlist(train), ncol = 257, byrow = TRUE)
+y = train[, 1]
+x = train[, -1]
+
+###############     FUNCTION     #################
+k = c(1,3,5,7,15) #define knn's k's candidate
+
+knncv <- function(x, y, fs, k) {
+  m = nrow(x)
+  fdsize = floor(m / fs) # #of elements in first (fs-1) fold
+  lastsize = m - fdsize * (fs - 1) # elements in last fold
+  firstfold = rep(1:(fs - 1), fdsize)
+  lastfold = rep(fs, lastsize)
+  set.seed(123)
+  ind = sample(c(firstfold, lastfold)) #randomly assign the 1-10fold indicator
+                                       #to dataset
+  mse = matrix(0,fs*length(k),4) #k,fs,train_mse,test_mse
+  m_ind=0
+   for (i in 1:length(k)) {
+     for (j in 1:fs) {
+      # for (i in 1:1) {
+      #   for (j in 1:10) {    
+       j_ind=which(ind == j)
+       #print(j_ind)
+       train_x = x[-j_ind,-1]
+       train_y = x[-j_ind,1]
+       test_x = x[j_ind,-1]
+       test_y = x[j_ind,1]
+      # print(c(nrow(train_y),nrow(train_x),nrow(test_x)))
+       test_yhat = knn(train_x, test_x, cl = factor(train_y), k = k[i])
+       train_yhat = knn(train_x, train_x, cl = factor(train_y), k = k[i])
+      # 
+       train_err = mean(train_yhat != train_y)
+       test_err = mean(test_yhat != test_y)
+       print(c(train_err,test_err))
+      m_ind=m_ind + 1
+      print(c(i,j,m_ind))
+      mse[m_ind,1]=k[i]
+      mse[m_ind,2]=j
+      mse[m_ind,3]=train_err
+      mse[m_ind,4]=test_err
+    }
+  }
+  return(mse)
 }
-
-p=ncol(train)
-train<-matrix(unlist(train), ncol = p, byrow = TRUE)
-y=train[,1]
-x=train[,-1]
-
-set.seed(123)
-cv<-function(x,y,fs,k){
-  m=nrow(x)
-  #temp=x
-  fdsize=floor(m/fs)
-  trainsize=fdsize*(fs-1)
-  testsize=m-fdsize*(fs-1)
-  ind=sample(1:m,trainsize,replace=FALSE)
-  
-  train_x=x[,ind]
-  train_y=x[,ind]
-  test_x=x[,-ind]
-  test_y=x[,-ind]
-  
-  test_yhat=knn(train_x,test_x,cl=factor(train_y),k = k)
-  train_yhat=knn(train_x,train_x,cl=factor(train_y),k = k)
-  
-  train_err= mean(train_yhat != test_yhat)
-  test_err=mean(test_yhat != test_yhat)
-}
-
-
+msetest<-knncv(x,y,10,k)
